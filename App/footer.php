1cb4e99a7aa9eb5578e7fa1db00351d7b46bebfb
filipemota130 <filipesimoesmotao@gmail.com>
2021-08-09 01:42:53 -0300

    inserindo footer

--- App/footer.php
+++ App/footer.php
@@ -1,3 +1,20 @@
+<footer>
+  <!-- Section: Social media -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://github.com/filipemota130">Filipe Mota</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+<style>
+  footer{
+    width: 100%;
+    position : absolute;
+    bottom : 0;
+  }
+</style>
+<!-- Footer -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="sweetalert2.all.min.js"></script>
